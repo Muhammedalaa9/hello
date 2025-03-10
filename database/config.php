d2a8afdda866b291a402935cd72f1476ea13e5e3
sujeongcrystal <ddujeoung@naver.com>
2025-03-10 09:39:42 +0900

    Delete database/config.php

--- database/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Configuration for database connection
-
-$host       = getenv('AZURE_MYSQL_HOST');
-$username   = getenv('AZURE_MYSQL_MYSQL_96028_USERNAME');
-$password   = getenv('AZURE_MYSQL_MYSQL_96028_PASSWORD');
-$db_name     = getenv('AZURE_MYSQL_MYSQL_96028_DBNAME');
-$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
