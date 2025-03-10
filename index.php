85123fc107c4f4b9ca0332ce2d5f1ebe0f316515
sujeongcrystal <ddujeoung@naver.com>
2025-03-10 09:09:22 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
+<?php include "templates/header.php"; ?>
 
-echo "Hello Azure!";
+<div class="center-align">
+	<ul>
+		<div class="left-align">
+			<li><a href="read.php"><strong>View Catalog</strong></a></li>
+			<li><a href="insert.php"><strong>Add a Product</strong></a></li>
+			<li><a href="update.php"><strong>Update a Product</strong></a></li>
+			<li><a href="delete.php"><strong>Remove a Product</strong></a></li>
+		</div>
+	</ul>
+</div>
+
+
+<?php include "templates/footer.php"; ?>
